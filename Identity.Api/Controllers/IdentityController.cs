161ff7be98b44e9a6047312f09f7afca837efc50
Dan Rose <dan@reddog32.com>
2025-03-25 18:34:06 +0000

    update

---
+++ Identity.Api/Controllers/IdentityController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+
+namespace Identity.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        private const string TokenSecret = "QWxhZGRpbpoisgTjpvcGVuIHNlc2FtZQ==";
+        private static readonly TimeSpan TokenLifetime = TimeSpan.FromHours(1);
+
+        [HttpPost("token")]
+        public IActionResult GenerateToken(
+            [FromBody]TokenGenerationRequest request)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(TokenSecret);
+
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new(JwtRegisteredClaimNames.Sub, request.Email!),
+                new(JwtRegisteredClaimNames.Email, request.Email!),
+                new("userid", request.UserId.ToString()),
+            };
+
+            foreach (var claimPair in request.CustomClaims)
+            {
+                var jsonElement = (JsonElement)claimPair.Value;
+                var valueType = jsonElement.ValueKind switch
+                {
+                    JsonValueKind.True => ClaimValueTypes.Boolean,
+                    JsonValueKind.False => ClaimValueTypes.Boolean,
+                    JsonValueKind.Number => ClaimValueTypes.Double,
+                    _ => ClaimValueTypes.String,
+                };
+
+                var claim = new Claim(claimPair.Key, claimPair.Value.ToString()!, valueType);
+                claims.Add(claim);
+            }
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.Add(TokenLifetime),
+                Issuer = "https://localhost:7209",
+                Audience = "https://localhost:7200",
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256),
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwt = tokenHandler.WriteToken(token);
+
+            return Ok(jwt);
+        }
+
+    }
+}
---
+++ Identity.Api/TokenGenerationRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace Identity.Api
+{
+    public class TokenGenerationRequest
+    {
+        public string? Email { get; set; }
+        public Guid UserId { get; set; }
+        public Dictionary<string, JsonElement> CustomClaims { get; set; } = [];
+    }
+
+}
