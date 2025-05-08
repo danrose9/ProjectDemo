0e9031658e12dc9bbd26c4351dd969ef23170ae1
Dan Rose <dan@reddog32.com>
2025-05-08 10:18:25 +0100

    Refactor GuidTest Controller

--- ProjectDemo.API/Controllers/GuidTestController.cs
+++ ProjectDemo.API/Controllers/GuidTestController.cs
@@ -1,19 +1,39 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared;
 using ProjectDemoApi.Services;
 
 namespace ProjectDemoApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GuidTestController(IEnumerable<IGuidService> guidServices) : ControllerBase
+    public class GuidTestController : ControllerBase
     {
-        private readonly IEnumerable<IGuidService> _guidServices = guidServices;
+        private readonly IEnumerable<IGuidService> _guidServices;
+        private readonly IServiceProvider _provider;
+
+        public GuidTestController(IEnumerable<IGuidService> guidServices, IServiceProvider provider)
+        {
+            _guidServices = guidServices;
+            _provider = provider;
+        }
 
         [HttpGet]
         public IActionResult GetGuids()
         {
             return Ok(_guidServices.Select(s => new {Type = s.GetType().Name, Guid = s.GetGuid() }));
+        }
+
+        [HttpGet("/api/RequiredService")]
+        public IActionResult GetRequiredService()
+        {
+            // .GetRequiredService<IGuidService>() will only return the last registered implementation,
+            // and if you want multiple implementations, you either:
+            // Use different interfaces, or
+            // Inject IEnumerable<T> to work with all at once.
+
+            var service = _provider.GetRequiredService<IGuidService>();
+            return Ok(new { Guid = service.GetGuid() });
         }
     }
 }
