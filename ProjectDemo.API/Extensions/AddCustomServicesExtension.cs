2a8a8a4ca2829123b8d6aefaf79541be24f8bbd5
Dan Rose <dan@reddog32.com>
2025-05-27 11:55:35 +0100

    added summary to AddCustomServicesExtension

--- ProjectDemo.API/Extensions/AddCustomServicesExtension.cs
+++ ProjectDemo.API/Extensions/AddCustomServicesExtension.cs
@@ -4,6 +4,17 @@ namespace ProjectDemoApi.Extensions
 {
     public static class AddCustomServicesExtension
     {
+
+        /// <summary>
+        /// Extension method to add custom services to the IServiceCollection.
+        /// 
+        /// Transient scope, new instance is registered every time
+        /// Scoped scope, new instance is registered per request
+        /// Singleton scope, new instance is registered once and shared across the application lifetime
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
         public static IServiceCollection AddCustomServices(this IServiceCollection services)
         {
             services.AddTransient<IGuidService, TransientGuidService>()
